869dec0c5a567624f7b3a87ace474768a62cc504
Guillem Rotger <grotger@kitmaker.com>
2017-01-02 18:12:54 +0100

    ;P

--- manage.py
+++ manage.py
@@ -4,8 +4,15 @@ from flask_script import Manager
 
 from project.server import PiCloud
 
+from project.server.extensions import sql
+
 app     = PiCloud()
 manager = Manager(app)
+
+@manager.command
+def create_db():
+    """Creates the sql tables."""
+    sql.create_all()
 
 if __name__ == "__main__":
     manager.run()
